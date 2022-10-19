70455af418990a1c97cd8e04c7f8c92aedca8700
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-19 16:38:44 -0700

    add links and naigation for table of contents

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# ReadME-Generator{"name":"peter","email":"peter@.com","project":"readme","description":"generate","license":"mit","dependencies":"nmp i","tests":"npm test","repo":"nothing","contributing-repo":"nada"}
+### Title: burger
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+## Description
+making a burger
+## Table of Contents:
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#test)
+- [Questions](#questions)
+## Installation
+npm i
+## Usage
+dont get hungry
+## License
+MIT
+## Contributing
+make it a smash burger
+## Test
+npm test
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at peter@ymail.com. You can find more of my work at [peter](link here).
--- index.js
+++ index.js
@@ -51,21 +51,21 @@ inquirer
 
 function formatData(data) {
     let output = "### Title: " + data.project + "\n";
-    output += "[![License: MIT](https://img.shields.io/badge/License-" + data.license + "-blue.svg)](https://opensource.org/licenses/" + data.license + ")"
+    output += "[![License: MIT](https://img.shields.io/badge/License-" + data.license + "-blue.svg)](https://opensource.org/licenses/" + data.license + ")";
     output += "## Description\n" + data.description + "\n";
     output += "## Table of Contents:\n" 
-    output += "- [Installation](installation)\n";
-    output += "- [Usage](usage)\n";
-    output += "- [License](license)\n";
-    output += "- [Contributing](contributing)\n";
-    output += "- [Tests](test)\n";
-    output += "- [Questions](questions)\n";
+    output += "- [Installation](#installation)\n";
+    output += "- [Usage](#usage)\n";
+    output += "- [License](#license)\n";
+    output += "- [Contributing](#contributing)\n";
+    output += "- [Tests](#test)\n";
+    output += "- [Questions](#questions)\n";
     output += "## Installation\n" + data.dependencies + "\n";
     output += "## Usage\n" + data.repo + "\n";
     output += "## License\n" + data.license + "\n";
     output += "## Contributing\n" + data.contributing + "\n";
     output += "## Test\n" + data.tests + "\n";
-    output += "## Questions\n" + "If you have any questions about the repo, open an issue or contact me directly at " + data.email + ". You can find more of my work at " + data.name + "." + "\n"
+    output += "## Questions\n" + "If you have any questions about the repo, open an issue or contact me directly at " + data.email + ". You can find more of my work at [" + data.name + "](https://github.com/" + data.name + ")." + "\n"
    
 
     return output;
