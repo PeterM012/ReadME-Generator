9c43643f8b2a81450a4d3ac4cae2d28fd317f3d8
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-19 16:23:56 -0700

    add outputs for readMe

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ inquirer
         name: 'description',
     },
     {
+        type: "list",
         message: "What kind of license should your project have?",
         name: 'license',
+        choices: ['MIT', 'GNU', 'Apache 2.0', 'ISC'],
     },
     {
         message: "What command should be run to install dependencies?",
@@ -42,23 +44,29 @@ inquirer
 ])
 
 .then((response) => {
-    fs.appendFile('README.md', formatData(response), (err) =>{
+    fs.writeFile('README.md', formatData(response), (err) =>{
         err ? console.error(err): console.log('Success!')
     })
 });
 
 function formatData(data) {
-    let output = "Github UserName:\n" + data.name + "\n";
-    output += "Github Email: " + data.email + "\n";
-
-    
-    output += "Title: " + data.project + "\n";
-    output += "Description: " + data.description + "\n";
-    output += "License: " + data.license + "\n";
-    output += "Dependencies: " + data.dependencies + "\n";
-    output += "Test: " + data.tests + "\n";
-    output += "Test: " + data.repo + "\n";
-    output += "Test: " + data.contributing + "\n";
+    let output = "### Title: " + data.project + "\n";
+    output += "[![License: MIT](https://img.shields.io/badge/License-" + data.license + "-blue.svg)](https://opensource.org/licenses/" + data.license + ")"
+    output += "## Description\n" + data.description + "\n";
+    output += "## Table of Contents:\n" 
+    output += "- [Installation](installation)\n";
+    output += "- [Usage](usage)\n";
+    output += "- [License](license)\n";
+    output += "- [Contributing](contributing)\n";
+    output += "- [Tests](test)\n";
+    output += "- [Questions](questions)\n";
+    output += "## Installation\n" + data.dependencies + "\n";
+    output += "## Usage\n" + data.repo + "\n";
+    output += "## License\n" + data.license + "\n";
+    output += "## Contributing\n" + data.contributing + "\n";
+    output += "## Test\n" + data.tests + "\n";
+    output += "## Questions\n" + "If you have any questions about the repo, open an issue or contact me directly at " + data.email + ". You can find more of my work at " + data.name + "." + "\n"
+   
 
     return output;
 }
